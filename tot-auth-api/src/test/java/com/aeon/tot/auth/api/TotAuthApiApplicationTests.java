d1133efab709bc2a0c0925522a18f77ce66a3e68
aeon <fabio-trab@outlook.com>
2024-01-31 21:22:44 -0400

    first commit

---
+++ tot-auth-api/src/test/java/com/aeon/tot/auth/api/TotAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.auth.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
