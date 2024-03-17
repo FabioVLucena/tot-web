3c8532fa9d17f48b9f3b5091f713450cba247343
aeon <fabio-trab@outlook.com>
2024-03-17 09:57:23 -0400

    feat: create project tot profile api

---
+++ tot-profile-api/src/main/java/com/aeon/tot/profile/api/TotProfileApiApplication.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.profile.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotProfileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotProfileApiApplication.class, args);
+	}
+
+}
