233be45e696a0fb980b2ca8efac5c668d6c4adda
aeon <fabio-trab@outlook.com>
2024-03-18 20:01:07 -0400

    feat: create project eureka server

---
+++ tot-eureka-server/src/main/java/com/aeon/tot/eureka/server/TotEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotEurekaServerApplication.class, args);
+	}
+
+}
---
+++ tot-eureka-server/src/test/java/com/aeon/tot/eureka/server/TotEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
