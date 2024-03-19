f04e891f4dc350908981961fa4fa811aa25b233f
aeon <fabio-trab@outlook.com>
2024-03-18 20:21:12 -0400

    feat: connect microservices to eureka server

--- tot-eureka-server/src/main/java/com/aeon/tot/eureka/server/TotEurekaServerApplication.java
+++ tot-eureka-server/src/main/java/com/aeon/tot/eureka/server/TotEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.aeon.tot.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class TotEurekaServerApplication {
 
