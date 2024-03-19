63be86aad399608ee4b3ba6b39565378121a54de
aeon <fabio-trab@outlook.com>
2024-03-18 20:22:02 -0400

    feat: create project cloud gateway

---
+++ tot-cloud-gateway/src/main/java/com/aeon/tot/cloud/gateway/TotCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotCloudGatewayApplication.class, args);
+	}
+
+}
