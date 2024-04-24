2a2629f01c883208264ec7342d3f5d32ca4233c0
aeon <fabio-trab@outlook.com>
2024-04-24 19:32:14 -0400

    feat: add photo url to get profile response

--- tot-profile-api/src/main/java/com/aeon/tot/profile/api/dto/GetProfileByIdResponse.java
+++ tot-profile-api/src/main/java/com/aeon/tot/profile/api/dto/GetProfileByIdResponse.java
@@ -2,10 +2,13 @@ package com.aeon.tot.profile.api.dto;
 
 import java.time.LocalDate;
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.aeon.tot.profile.api.entity.Profile;
 
 public record GetProfileByIdResponse(Long id, Long userId, String name,
-		String lastName, LocalDate birthDate, String biography) {
+		String lastName, LocalDate birthDate, String biography,
+		String photoUrl) {
 
 	public static GetProfileByIdResponse convert(Profile profile) {
 		Long id = profile.getId();
@@ -15,7 +18,11 @@ public record GetProfileByIdResponse(Long id, Long userId, String name,
 		LocalDate birthDate = profile.getBirthDate();
 		String biography = profile.getBiography();
 		
+		String photoUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/v1/profiles/" + id + "/photo")
+				.toUriString();
+		
 		return new GetProfileByIdResponse(id, userId, name,
-				lastName, birthDate, biography);
+				lastName, birthDate, biography, photoUrl);
 	}
 }
