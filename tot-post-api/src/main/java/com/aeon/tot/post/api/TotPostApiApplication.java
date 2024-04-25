12d4a7e4e5a20c14ff990396690c14cbf848ed86
aeon <fabio-trab@outlook.com>
2024-04-25 09:06:45 -0400

    feat: create project post api

---
+++ tot-post-api/src/main/java/com/aeon/tot/post/api/TotPostApiApplication.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.post.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotPostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotPostApiApplication.class, args);
+	}
+
+}
---
+++ tot-post-api/src/test/java/com/aeon/tot/post/api/TotPostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeon.tot.post.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotPostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
